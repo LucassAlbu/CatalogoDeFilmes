6af0731f1731c76f637bbf929098bdd0364e7198
LucassAlbu <lucassilva.alb@hotmail.com>
2022-11-01 15:10:05 -0300

    ViewBinding Setado

--- app/src/main/java/com/lucassalbu/catalogodefilmessuperior/ui/MainActivity.kt
+++ app/src/main/java/com/lucassalbu/catalogodefilmessuperior/ui/MainActivity.kt
@@ -2,11 +2,20 @@ package com.lucassalbu.catalogodefilmessuperior.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.lucassalbu.catalogodefilmessuperior.R
+import com.lucassalbu.catalogodefilmessuperior.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
     }
 }
