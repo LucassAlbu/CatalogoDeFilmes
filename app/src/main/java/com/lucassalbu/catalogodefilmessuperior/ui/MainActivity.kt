9acbc0c25ac69889405652aab4781af56a810ecf
LucassAlbu <lucassilva.alb@hotmail.com>
2022-11-01 14:10:42 -0300

    add fragments de login

--- app/src/main/java/com/lucassalbu/catalogodefilmessuperior/ui/MainActivity.kt
+++ app/src/main/java/com/lucassalbu/catalogodefilmessuperior/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.lucassalbu.catalogodefilmessuperior
+package com.lucassalbu.catalogodefilmessuperior.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lucassalbu.catalogodefilmessuperior.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
